97d8872f8ce2be3b83ffd3eae73d00beaf07d10b
charles <charles76680119@gmail.com>
2021-10-15 07:37:55 +0800

    UI : UI modify
    1.NavigationBar search button replace to test button
    2.Novel cover size modify

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,30 +1,25 @@
 // import { button } from "react-bootstrap";
 import React from "react";
-import {Button} from "react-bootstrap";
 import {Redirect} from "react-router-dom";
 import "./NavigationBar.css";
+import {Button} from "react-bootstrap";
 
 class NavigationBar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {redirect: false};
-        this.changePage = this.changePage.bind(this);
-    }
-
-    changePage() {
-        this.setState({redirect: true});
     }
 
     render() {
         const navigator = (
-            <div id="navigation_bar">
+            <div id="NavigationBar">
                 <div id='slide_nav'>
                     <p id="slide_nav_button"
                        onClick={() => this.props.sidePanelVisibleChange(!this.props.sidePanelVisible)}>☰</p>
                 </div>
                 {/*<Button onClick={() => this.props.visibleChange(true)}>☰</Button>*/}
                 <input placeholder="input search text"></input>
-                <Button onClick={this.changePage}>test</Button>
+                <Button>Search</Button>
             </div>
         );
         const redirect = <Redirect to={this.props.location}></Redirect>;
