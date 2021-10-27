b0e268d5bee4f5b6a5d0d8a0f07b43b0dfd8d75c
charles <charles76680119@gmail.com>
2021-10-27 13:23:52 +0800

    build : set environment when test electron

--- main.js
+++ main.js
@@ -5,20 +5,27 @@ const {spawn} = require('child_process')
 let server_process
 
 function createWindow() {
-    const win = new BrowserWindow({width: 800, height: 600})
+    const win = new BrowserWindow({width: 800, height: 600 , autoHideMenuBar:true})
     const readCallBack = (err, data) => {
-        server_process = spawn("server.exe")
-        server_process.on('spawn', () => {
-            let url = ''
-            if (err) {
-                console.log(err)
-            } else {
-                let jsonData = JSON.parse(data)
-                url = jsonData.url.toString()
-                console.log(url)
-            }
-            win.loadURL(url).then(r => r)
-        })
+        if (process.env.NODE_SERVER_PATH !== undefined) {
+            const url = process.env.NODE_SERVER_PATH
+            // noinspection JSIgnoredPromiseFromCall
+            win.loadURL(url)
+        } else {
+            server_process = spawn("server.exe")
+            server_process.on('spawn', () => {
+                let url = ''
+                if (err) {
+                    console.log(err)
+                } else {
+                    let jsonData = JSON.parse(data)
+                    url = jsonData.url.toString()
+                    console.log(url)
+                }
+                // noinspection JSIgnoredPromiseFromCall
+                win.loadURL(url)
+            })
+        }
     }
     fs.readFile('server.json', readCallBack)
 }
