cffd8e07bfb82f2cc2fd6f2686716ac69f85bbd0
charles <charles76680119@gmail.com>
2021-10-28 16:49:41 +0800

    UI : modify UI

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,25 +1,74 @@
 // import { button } from "react-bootstrap";
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 import "./css/NavigationBar.css";
 import {Button} from "react-bootstrap";
-import SlideOutPanel from "./SlideOutPanel";
+import {Box, IconButton, Input, Menu, MenuButton, MenuItem, MenuList} from "@chakra-ui/react";
+import {HamburgerIcon} from "@chakra-ui/icons";
+import LoadingPage from "./LoadingPage";
 
 function NavigationBar() {
-    const [sideVisible, setSideVisible] = useState(false)
     const search = () => {
         const element = document.querySelector("#NavigationBar > input");
         window.updateNovelList(element.value);
     }
     return (
-        <div className="NavigationBar">
-            <div className="SlideNavSwitch">
-                <p onClick={() => setSideVisible(!sideVisible)}>☰</p>
-            </div>
-            {sideVisible && <SlideOutPanel/>}
-            <input placeholder="input search text to search title"/>
+        <Box className="NavigationBar" paddingTop={'1px'}>
+            <SettingMenu/>
+            <Input placeholder="input search text to search title" border={'1px solid'} borderColor={'black'}/>
             <Button onClick={search}>Search</Button>
-        </div>
+        </Box>
     );
+}
+
+function SettingMenu() {
+    const [loading, setLoading] = useState(false)
+    const uploadRef = useRef()
+    const startUpload = (event) => {
+        setLoading(true)
+        const formData = new FormData()
+        formData.append('file', event.target.files[0])
+
+        const options = {
+            method: 'POST',
+            body: formData,
+        }
+        console.log(formData)
+        fetch(window.serverURL + "novels", options).then(response => ({
+            status: response.status,
+            data: response
+        })).then(data => {
+            if (data.status === 208) {
+                alert('file exist')
+            } else if (data.status !== 200) {
+                alert('error')
+            } else {
+                data.data.json().then(() => {
+                    window.updateNovelList()
+                    alert('success')
+                })
+            }
+            setLoading(false)
+        }).catch((err) => {
+            alert(err)
+            setLoading(false)
+        })
+    }
+
+    return (
+        <Menu>
+            <MenuButton as={IconButton} icon={<HamburgerIcon/>} color={'black'} height={'100%'}/>
+            <MenuList>
+                <MenuItem> Setting </MenuItem>
+                <MenuItem onClick={(e) => {
+                    e.preventDefault()
+                    // noinspection JSUnresolvedFunction
+                    uploadRef.current.click()
+                }}>Upload</MenuItem>
+            </MenuList>
+            <Input type="file" accept=".txt" display={'none'} ref={uploadRef} onChange={startUpload}/>
+            {loading === true && <LoadingPage text={"uploading..."}/>}
+        </Menu>
+    )
 }
 
 export default NavigationBar;
